8572c005b07b94feb380c548e077352db5fc6b32
Abigail <abi25.fly@gmail.com>
2017-09-19 16:50:01 -0500

    ready

---
+++ main.js
@@ -0,0 +1,65 @@
+var l1=[1,2,3];
+var l2=[2,3,4];
+// Forma Imperativa
+function intersection(l1, l2){
+  var l3=[];
+  for(var i=0; i<l1.length; i++){
+    for(var j=0; j<l2.length; j++){
+      if(l1[i]==l2[j]){
+        l3.push(l1[i]);
+      }
+    }
+  }
+  return l3;
+}
+console.log(intersection(l1,l2));
+
+// forma declarativa
+function intersectionSet2(l1, l2){
+  return l1.filter(function(n){
+    return l2.indexOf(n) !== -1
+  });
+};
+console.log(intersectionSet2(l1, l2));
+
+var productos = [
+  {
+    nombre:"pijama",
+    tipo:"ropa"
+  },
+  {
+    nombre:"nevera",
+    tipo: "electrodoméstico"
+  },
+  {
+    nombre:"asus",
+    tipo:"computador"
+  },
+  {
+    nombre:"macbook",
+    tipo:"computador"
+  },
+  {
+    nombre:"pijama",
+    tipo:"ropa"
+  },
+  {
+    nombre: "toshiba",
+    tipo:"computador"
+  }
+]
+
+// forma Imperativa
+var computadores = [];
+for(var i = 0; i< productos.length; i++){
+  if(productos[i].tipo=== "computador"){
+    computadores.push(productos[i]);
+  }
+}
+console.log(computadores);
+
+// forma declarativa
+var computadores2 = productos.filter(function(producto){
+  return producto.tipo === "computador";
+});
+console.log(computadores2);
